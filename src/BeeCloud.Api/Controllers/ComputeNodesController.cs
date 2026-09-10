e6474c48b6e302ce7380b662c00724c713cd1401
Sahak Hovhannisyan <sahak_hovhannisyan@epam.com>
2026-09-10 14:42:41 +0400

    ComputeNodes Controller

---
+++ src/BeeCloud.Api/Controllers/ComputeNodesController.cs
@@ -0,0 +1,29 @@
+﻿using BeeCloud.Application.DTOs.ComputeNodes;
+using BeeCloud.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeeCloud.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/nodes")]
+public class ComputeNodesController : ControllerBase
+{
+    private readonly IComputeNodeService _service;
+
+    public ComputeNodesController(IComputeNodeService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ComputeNodeResponse>> Create(
+        [FromBody] CreateComputeNodeRequest request,
+        CancellationToken cancellationToken)
+    {
+        var response = await _service.CreateAsync(
+            request,
+            cancellationToken);
+
+        return Accepted(response);
+    }
+}
