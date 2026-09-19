637c2a643374677b0bb491ccf21fba8134e078b9
Sahak Hovhannisyan <sahak_hovhannisyan@epam.com>
2026-09-19 11:36:27 +0400

    adding isActive field to network and migrate to db

---
+++ src/BeeCloud.Infrastructure/Persistence/Migrations/20260919071556_AddNetworkActiveState.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeeCloud.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNetworkActiveState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "networks",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "networks");
+        }
+    }
+}
